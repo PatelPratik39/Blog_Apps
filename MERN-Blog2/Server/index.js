8e99124cec431b1c4bc603758ac7763229aa4508
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-03 02:22:39 -0500

    index.js

---
+++ MERN-Blog2/Server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const db = require("db");
+const postRouter = require("./Routes/post");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/posts", postRouter);
+
+app.listen(PORT, () => {
+  console.log(`app is listening to PORT ${PORT}`);
+});
